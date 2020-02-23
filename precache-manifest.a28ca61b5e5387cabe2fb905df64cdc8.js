000b795d26dcb90734969c73d8ac6ea57f9aab1e
Islamova Akmaral <akmaralslmv@gmail.com>
2020-02-23 20:13:26 +0600

    Updates

---
+++ precache-manifest.a28ca61b5e5387cabe2fb905df64cdc8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f0cf3dc2a0bb9b33aeca8685aa106ca",
+    "url": "/reactive-quiz-app/index.html"
+  },
+  {
+    "revision": "a2d64453fa544a7124f5",
+    "url": "/reactive-quiz-app/static/css/main.39016fc3.chunk.css"
+  },
+  {
+    "revision": "b4e80ab39d677f7ab2b7",
+    "url": "/reactive-quiz-app/static/js/2.22411106.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/reactive-quiz-app/static/js/2.22411106.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2d64453fa544a7124f5",
+    "url": "/reactive-quiz-app/static/js/main.40edceff.chunk.js"
+  },
+  {
+    "revision": "946bcc1dcdba743f08f8",
+    "url": "/reactive-quiz-app/static/js/runtime-main.3404f1a1.js"
+  }
+]);
